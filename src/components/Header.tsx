89e364b6b7c13489051b6030ce03282e8cc6dd88
Jade <jadebot.dev@gmail.com>
2025-12-07 16:24:50 +0800

    fix the tabIndex

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ export default function Home() {
                 </svg>
               </div>
               <ul
-                tabIndex="-1"
+                tab-index="-1"
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
                 <li>
